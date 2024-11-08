b0d48324e69fb6d9f85f56401e5e3cae6b4d01cf
Tikss22 <atikarehmandubai@gmail.com>
2024-11-08 19:56:31 +0400

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -1,8 +1,4 @@
-# Assign the string "Coding" to the variable word1
 word1 = "Coding"
-# Assign the string "is" to the variable word2
 word2 = "is"
-# Assign the string "Cool" to the variable word3
 word3 = "Cool"
-# Print the values of word1, word2, and word3, with spaces between them
 print(word1, word2, word3)
