68d758215b578c6de408d64cc41e6c1c253fbbf5
Tikss22 <atikarehmandubai@gmail.com>
2024-11-08 19:52:44 +0400

    Update Exercise 2.py

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,8 +1,4 @@
-# Assign the value 8 to the variable a
 a = 8
-# Assign the value 10 to the variable b
 b = 10
-# Add the values of a and b, and assign the result to c
 c = a + b
-# Print the value of c, which is the sum of a and b
 print(c)
