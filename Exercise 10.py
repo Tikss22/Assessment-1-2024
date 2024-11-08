c2fa77a588da3f3aff0387690811aab14e9e3a03
Tikss22 <atikarehmandubai@gmail.com>
2024-11-08 19:52:11 +0400

    Update Exercise 10.py

--- Exercise 10.py
+++ Exercise 10.py
@@ -1,11 +1,7 @@
 def main():
-    # Step 1: Ask the user to input a number and convert it to an integer
     number = int(input("Enter a number: "))
-     # Step 2: Check if the number is even (i.e., remainder when divided by 2 is 0)
     if number % 2 == 0:
-    # Step 3: If the number is even, print this message
      print("The entered number is an even number")
     else:
-    # Step 4: If the number is odd, print this message
      print("The entered number is an odd number")
 main()
